2dd37a09521b6c559ad0f0172ac56148bfe10201
unknown <tagaanjustinemark3@gmail.com>
2024-11-21 07:37:07 +0800

    fix: holdings data

--- front-end/js/holding-details/holding.js
+++ front-end/js/holding-details/holding.js
@@ -17,11 +17,18 @@ $(document).ready(function () {
             if (response.data.suggested_books.length > 0) {
                 $.map(response.data.suggested_books, function (element, index) {
                     suggested_books += `
-                        <div class="bg-gray-50 hover:bg-gray-100 p-4 rounded-lg mt-2">
-                            <h4 class="holding-title font-bold">${index+1}. <span class="bg-gray-200 p-1 rounded-lg">${element.title.split(' ')[0]}</span> ${element.title.split(' ').slice(1).join(' ')}</h4>
-                            <div id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">
-                                <p><strong>Author: </strong>${element.author}</p>
-                                <p><strong>Year Published: </strong>${element.published_year}</p>
+                        <div class="bg-gray-50 hover:bg-gray-100 flex flex-wrap justify-between p-4 rounded-lg mt-2">
+                            <div>
+                                <h4 class="holding-title font-bold">${index+1}. <span class="bg-gray-200 p-1 rounded-lg">${element.title.split(' ')[0]}</span> ${element.title.split(' ').slice(1).join(' ')}</h4>
+                                <div id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">
+                                    <p><strong>Author: </strong>${element.author}</p>
+                                    <p><strong>Year Published: </strong>${element.published_year}</p>
+                                </div>
+                            </div>
+                            <div class="mt-2">
+                                <a href="book-details.html?holdingid=${element.hold_id}"> 
+                                    <button type="button" class="text-white bg-[#FEB440] hover:bg-[#3F1D22] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 md:px-5 md:py-2.5 me-2 mb-2 focus:outline-none">Book Details</button>   
+                                </a>
                             </div>
                         </div>
                     `
--- front-end/js/index/display.js
+++ front-end/js/index/display.js
@@ -80,7 +80,7 @@ $(document).ready(function () {
                     </div>
                     <div class="mt-2">
                         <a href="book-details.html?holdingid=${holding.hold_id}"> 
-                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 md:px-5 md:py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Book Details</button>   
+                            <button type="button" class="text-white bg-[#FEB440] hover:bg-[#3F1D22] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 md:px-5 md:py-2.5 me-2 mb-2 focus:outline-none">Book Details</button>   
                         </a>
                     </div>
                 </div>
